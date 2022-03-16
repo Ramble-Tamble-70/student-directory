70e5f55936e5ae59019ee6a4d98499299d0d032d
David Campbell <ewan.david@gmail.com>
2022-03-16 21:53:53 +0000

    Student directory Ex.8.1 each_with_index (& capitalize) refactor

--- directory.rb
+++ directory.rb
@@ -1,19 +1,16 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+  
   students = []
-  # get the first name
+  
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name.capitalize, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another name from the user
      name = gets.chomp
   end
-  # return the array of students
   students
 end
 
@@ -23,8 +20,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
